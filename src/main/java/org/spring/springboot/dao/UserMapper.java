6375e0b9fa801fa735f31faa93d543242663fee1
yangchunsunshine <1457140983@qq.com>
2017-11-30 22:14:27 +0800

    springBoot集成Mybatis 自动生成配置文件

---
+++ src/main/java/org/spring/springboot/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package org.spring.springboot.dao;
+
+import org.spring.springboot.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/org/spring/springboot/domain/User.java
@@ -0,0 +1,35 @@
+package org.spring.springboot.domain;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private Date date;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
